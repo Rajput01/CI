e209627a32444fd4212b20e98c86383919a3ad5a
Rajput01 <63243632+Rajput01@users.noreply.github.com>
2020-04-21 20:33:23 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,5 +3,4 @@ def test_multiply():
    result=main.multiply(3,4)
    result_2=main.multiply(2,3)
    print("running tests...")
-   assert result_2 == 4
-   assert result == 12
+   assert result == 12 #throws an error if result != 12
